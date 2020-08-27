e9c94b6b39308a0bbf0df503ba773a23099fe8ac
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2020-08-27 20:06:22 +0100

    added docker build

--- poc/sampledjango/mysite/mysite/settings.py
+++ poc/sampledjango/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@u9ide#-5sv*=b5)@*yo$&whp_lpu+4gvvowg&k*wv(j@*sd&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+NAME_VAR = "William"
